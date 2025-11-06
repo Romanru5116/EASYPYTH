da5e89df94571a38a08e8dc8b6f862637c21dd31
Roman <141589347+Romanru5116@users.noreply.github.com>
2025-11-07 01:35:26 +0300

    Update main.py
     набросок черновик

--- MVPSRVList/source/main.py
+++ MVPSRVList/source/main.py
@@ -1,1 +1,21 @@
+Import tkinter as tk
 
+def button_click():
+    """Function to be called when the button is clicked."""
+    label.config(text="Button was clicked!")
+
+# Create the main window
+root = tk.Tk()
+root.title("Tkinter Example")
+root.geometry("300x200") # Set initial window size
+
+# Create a Label widget
+label = tk.Label(root, text="Hello, Srvlist!")
+label.pack(pady=20) # Add padding for better spacing
+
+# Create a Button widget
+button = tk.Button(root, text="service1", command=button_click)
+button.pack()
+
+# Start the Tkinter event loop
+root.mainloop()
